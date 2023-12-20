b7e44a86b218e5612ec7a6fc05b7b720e64b4540
AreejAbdulaziz <148686069+AreejAbdulaziz@users.noreply.github.com>
2023-12-20 13:14:48 +0300

    Add files via upload

---
+++ main/java/com/example/capstone3/Repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.example.capstone3.Repository;
+
+import com.example.capstone3.Model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Integer> {
+    Member findMemberById(Integer id);
+
+    @Query("select m from Member m where m.role=?1")
+    Member findMembersByRole(String Leader);
+
+    Member findMemberByEmail(String email);
+
+//    List<Member> findMembersByA
+}
